4141d61a9a125e3f879627a98eb4698a297957e5
Stefan Niederhauser <ghuder5@gmx.ch>
2016-12-23 20:19:13 +0100

    add initial version

--- README.md
+++ README.md
@@ -1,2 +1,66 @@
 # js-grid-layout
-A simple grid layout with CSS and JS
+A simple grid layout with CSS and JS.
+
+# Usage
+````   
+<div class="border-layout">
+
+    <div class="north resizable" size="50px" min-size="10px" max-size="30%">
+        <div class="content">
+            This is north
+        </div>
+    </div>
+
+    <div class="west">
+        <div class="content">
+            This is west
+        </div>
+    </div>
+    
+    <div class="east">
+        <div class="content">
+            This is east
+        </div>
+    </div>
+    
+    <div class="south">
+        <div class="content">
+            This is south
+        </div>
+    </div>
+    
+    <div class="center">
+        <div class="content">
+            And the center
+        </div>
+    </div>
+</div>
+
+<script>
+    var layout = borderLayout(document.querySelector('.border-layout'));
+</script>
+````
+
+# Configuration
+
+The `north`, `east`, `south`, `west`, `center` divs can have 
+- `resizable` class to make it resizable by the mouse.
+- `size`, `min-size`, `max-size` attributes to define the initial, minimum and maximum size in `px` or `%`.
+
+# API
+| Initialize the layout | `var layout = borderLayout(domElement, resizeCallback: function(sizes: Sizes))` |
+| Resize the sections | `layout.resize(sizes: Sizes)` |
+| Hide a section | `layout.hide(section: Section)` |
+| Minimize a section | `layout.minimize(section: Section)` |
+| Maximize a section | `layout.maximize(section: Section)` |
+
+````
+type Sizes = Object{
+    north: number
+    south: number
+    west: number
+    east: number
+}
+
+type Secion = 'north' | 'south' | 'west' | 'east'
+````
---
+++ border.js
@@ -0,0 +1,150 @@
+var borderLayout = function (root, onStateChanged) {
+    var dragging;
+    var area = {
+        north: {element: root.querySelector('.north')},
+        south: {element: root.querySelector('.south')},
+        west: {element: root.querySelector('.west')},
+        east: {element: root.querySelector('.east')},
+        center: {element: root.querySelector('.center')}
+    };
+    setInitSize('north', root.offsetHeight);
+    setInitSize('south', root.offsetHeight);
+    setInitSize('west', root.offsetWidth);
+    setInitSize('east', root.offsetWidth);
+    redraw();
+    addDrag('north', {left: '0', bottom: '0', width: '100%', cursor: 'ns-resize'}, function (e) {
+        return this.origSize + e.clientY - this.origY;
+    });
+    addDrag('south', {left: '0', top: '0', width: '100%', cursor: 'ns-resize'}, function (e) {
+        return this.origSize - e.clientY + this.origY;
+    });
+    addDrag('west', {right: '0', top: '0', height: '100%', cursor: 'ew-resize'}, function (e) {
+        return this.origSize + e.clientX - this.origX;
+    });
+    addDrag('east', {left: '0', top: '0', height: '100%', cursor: 'ew-resize'}, function (e) {
+        return this.origSize - e.clientX + this.origX;
+    });
+    root.addEventListener('mousemove', function (e) {
+        if (dragging) {
+            var target = area[dragging.target];
+            target.size = Math.min(target.maxSize, Math.max(target.minSize, dragging.size(e)));
+            redraw();
+        }
+    });
+    root.addEventListener('mouseup', function (e) {
+        dragEnded(onStateChanged);
+    });
+    root.addEventListener('mouseout', function (e) {
+        if (e.target === root) {
+            dragEnded(onStateChanged);
+        }
+    });
+
+    return {
+        resize: resize,
+        maximize: maximize,
+        minimize: minimize,
+        hide: hide
+    };
+
+    function resize(size) {
+        for (var prop in size) {
+            area[prop].size = size[prop];
+        }
+        redraw();
+    }
+
+    function maximize(name) {
+        setSize(name, area[name].maxSize);
+    }
+
+    function minimize(name) {
+        setSize(name, area[name].minSize);
+    }
+
+    function hide(name) {
+        setSize(name, 0);
+    }
+
+    function setSize(name, size) {
+        area[name].size = size;
+        redraw();
+    }
+
+    function setInitSize(name, parent) {
+        var elem = area[name].element;
+        if (elem) {
+            area[name].size = unitToPx(elem.getAttribute('size') || '20%', parent);
+            area[name].minSize = unitToPx(elem.getAttribute('min-size') || '5px', parent);
+            area[name].maxSize = unitToPx(elem.getAttribute('max-size') || '40%', parent);
+        }
+    }
+
+    function unitToPx(value, parent) {
+        if (endsWith(value, 'px')) {
+            return parseInt(value);
+        }
+        if (endsWith(value, '%')) {
+            return parseInt(value) / 100 * parent;
+        }
+    }
+
+    function endsWith(s, end) {
+        return s.substring(s.length - end.length) === end;
+    }
+
+    function redraw() {
+        setSizeAttr('north', {height: 'north'});
+        setSizeAttr('south', {height: 'south'});
+        setSizeAttr('west', {width: 'west', paddingTop: 'north', paddingBottom: 'south'});
+        setSizeAttr('east', {width: 'east', paddingTop: 'north', paddingBottom: 'south'});
+        setSizeAttr('center', {paddingTop: 'north', paddingLeft: 'west', paddingBottom: 'south', paddingRight: 'east'});
+    }
+
+    function setSizeAttr(target, value) {
+        var elem = area[target].element;
+        if (elem) {
+            for (var prop in value) {
+                elem.style[prop] = area[value[prop]].size + 'px';
+            }
+        }
+    }
+
+    function getSize(target, attr) {
+        var elem = area[target].element;
+        return elem && parseInt(elem[attr]);
+    }
+
+    function addDrag(name, style, sizeFn) {
+        var target = area[name].element;
+        if (target && target.classList.contains('resizable')) {
+            var drag = document.createElement('div');
+            drag.className = 'drag';
+            Object.assign(drag.style, style);
+            target.appendChild(drag);
+            drag.addEventListener('mousedown', function (e) {
+                dragging = {
+                    target: name,
+                    origX: e.clientX,
+                    origY: e.clientY,
+                    origSize: area[name].size
+                };
+                dragging.size = sizeFn.bind(dragging);
+            });
+        }
+    }
+
+    function dragEnded(listener) {
+        if (dragging) {
+            if (listener) {
+                listener({
+                    north: getSize('north', 'offsetHeight'),
+                    south: getSize('south', 'offsetHeight'),
+                    west: getSize('west', 'offsetWidth'),
+                    east: getSize('east', 'offsetWidth')
+                });
+            }
+            dragging = null;
+        }
+    }
+};
